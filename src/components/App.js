3a6118acbd02ab5be6890e51ba0be914a1db6a09
Abhishek <28715657+proabhishek@users.noreply.github.com>
2023-08-13 08:24:32 +0000

    title

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,11 @@ import './../styles/App.css';
 import Tabs from './Tabs';
 
 
-const tabs = [
-{titles:"Tab 1" , contents:"Tab 1"},
-
-{titles:"Tab 2" , contents:"Tab 2"},
-
-{titles:"Tab 3" , contents:"Tab 3"}
-]
+const tabsData = [
+  { titles: 'Tab 1', contents: 'This is the content for Tab 1.'  },
+  { titles: 'Tab 2', contents: 'This is the content for Tab 2.'  },
+  { titles: 'Tab 3', contents: 'This is the content for Tab 3.' },
+];
 
 const App = () => {
   return (
